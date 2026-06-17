8807514d592afc8a05b77559edf19a7c32d222e1
Bahar <berlyna05@gmail.com>
2026-06-18 00:46:27 +0330

    Implement controller outputs for datapath operations

--- AveController.v
+++ AveController.v
@@ -1,6 +1,8 @@
-module average (
-    input [7:0]dataIn, start, reset, clk,
-    output [15:0]dataOut, dataReady
+module AveController (
+    input [7:0]dataIn,
+    input start, reset, clk,
+    output [15:0]dataOut,
+    output dataReady, rstsum, loadsum, rstcounter, encount, rstout, loadoutput
 );
 
 parameter [1:0]
@@ -15,32 +17,43 @@ always @(posedge clk)
 begin
     if (reset)
         Pstate <= IDLE;
-    else:
+    else
         Pstate <= nstate;
 end
 
 always @(Pstate, start) begin
+    dataReady  = 1'b0;
+    rstsum     = 1'b0;
+    loadsum    = 1'b0;
+    rstcounter = 1'b0;
+    encount    = 1'b0;
+    rstout     = 1'b0;
+    loadoutput = 1'b0;
     nstate = IDLE;
     case(Pstate)
         IDLE:
+            rstsum = 1'b1;
+            rstcounter = 1'b1;
+            rstout = 1'b1;
             if(start)
                 nstate = GETData;
             else
                 nstate = IDLE;
         
         GETData:
+            loadsum = 1'b1;
+            encount = 1'b1;
             if(start)
                 nstate = GETData;
             else
                 nstate = LoadData;
         
         LoadData:
+            loadoutput = 1'b1;
             nstate = Done;
         
         Done:
+            dataReady = 1'b1;
             nstate = IDLE;
     endcase
 end
-
-assign dataReady = (Pstate==Done) ? 1'b1 : 1'b0;
-                     
